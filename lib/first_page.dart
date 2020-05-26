60a543fc2c236fd33a6075273c433d63d965a1f8
Matt <mattresetar@resocoder.com>
2020-05-26 19:04:51 +0200

    Finished project

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,5 +1,6 @@
 import 'package:custom_page_transitions_tutorial/second_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FirstPage extends StatelessWidget {
   @override
@@ -8,8 +9,14 @@ class FirstPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => SecondPage(),
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) {
+                return ListenableProvider(
+                  create: (context) => animation,
+                  child: SecondPage(),
+                );
+              },
+              transitionDuration: const Duration(seconds: 1),
             ),
           );
         },
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SecondPage extends StatelessWidget {
+  final Animation<double> transitionAnimation;
+
+  const SecondPage({
+    Key key,
+    this.transitionAnimation,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           Expanded(
-            child: Container(
+            child: SlidingContainer(
               color: Colors.red,
+              initialOffsetX: 1,
+              intervalStart: 0,
+              intervalEnd: 0.5,
             ),
           ),
           Expanded(
-            child: Container(
+            child: SlidingContainer(
               color: Colors.green,
+              initialOffsetX: -1,
+              intervalStart: 0.5,
+              intervalEnd: 1,
             ),
           ),
         ],
@@ -25,6 +39,51 @@ class SecondPage extends StatelessWidget {
         label: Text('Navigate Back'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class SlidingContainer extends StatelessWidget {
+  final double initialOffsetX;
+  final double intervalStart;
+  final double intervalEnd;
+  final Color color;
+
+  const SlidingContainer({
+    Key key,
+    @required this.initialOffsetX,
+    @required this.intervalStart,
+    @required this.intervalEnd,
+    @required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = Provider.of<Animation<double>>(context, listen: false);
+
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset(initialOffsetX, 0),
+            end: Offset(0, 0),
+          ).animate(
+            CurvedAnimation(
+              curve: Interval(
+                intervalStart,
+                intervalEnd,
+                curve: Curves.easeOutCubic,
+              ),
+              parent: animation,
+            ),
+          ),
+          child: child,
+        );
+      },
+      child: Container(
+        color: color,
+      ),
     );
   }
 }
